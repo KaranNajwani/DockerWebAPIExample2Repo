476bf40d89210cc83bc2c964df2ed561957ab8e5
karan najwani <karannajwani777@gmail.com>
2022-03-10 14:54:01 +0530

    initial code checkin

---
+++ PaymentAPI/Controllers/PaymentDetailsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+        public PaymentDetailsController(PaymentDetailContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PaymentDetail>> GetPaymentDetails()
+        {
+            return await this._context.PaymentDetails.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetail>> GetPaymentDetail(int id)
+        {
+            var paymentDetail = await this._context.PaymentDetails.FindAsync(id);
+            return paymentDetail != null ? paymentDetail : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+        {
+            this._context.PaymentDetails.Add(paymentDetail);
+            await this._context.SaveChangesAsync();
+            return CreatedAtAction("GetPaymentDetails", new { id = paymentDetail.PaymentDetailId, paymentDetail });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PaymentDetail>> PutPaymentDetail(int id, PaymentDetail paymentDetail)
+        {
+            if (id != paymentDetail.PaymentDetailId)
+                return BadRequest();
+
+            this._context.Entry(paymentDetail).State = EntityState.Modified;
+
+            try
+            {
+                await this._context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaymentDetail>> DeletePaymentDetail(int id)
+        {
+            var paymentDetail = await this._context.PaymentDetails.FindAsync(id);
+            if (paymentDetail == null)
+                return NotFound();
+
+            this._context.PaymentDetails.Remove(paymentDetail);
+            await this._context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return this._context.PaymentDetails.Any(detail => detail.PaymentDetailId == id);
+        }
+    }
+}
---
+++ PaymentAPI/Models/PrepDB.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace PaymentAPI.Models
+{
+    public class PrepDB
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SendData(serviceScope.ServiceProvider.GetService<PaymentDetailContext>());
+            }
+        }
+
+        private static void SendData(PaymentDetailContext context)
+        {
+            Console.WriteLine("Applying migration...");
+            context.Database.Migrate();
+            if (context.PaymentDetails.Any())
+            {
+                context.PaymentDetails.AddRange(new PaymentDetail()
+                {
+                    CardNumber = "1234567891234567",
+                    CardOwnerName = "John Doe",
+                    ExpirationDate = "12/25",
+                    SecurityCode = "123"
+                });
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Already have data -- not seeding");
+            }
+        }
+    }
+}
